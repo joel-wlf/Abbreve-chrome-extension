54b3d6a58ffb4bc09e000fe0f987b04e88ec47d0
Joel <72318130+joel-wlf@users.noreply.github.com>
2023-12-04 20:40:08 +0100

    fix: hide overflow

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css'
 
 function App() {
   return (
-    <div className="bg-dark dark h-full max-h-screen max-w-screen sm:px-6 lg:px-8">
+    <div className="bg-dark dark h-full overflow-hidden max-h-screen max-w-screen sm:px-6 lg:px-8">
       <div className="flex w-full justify-center">
         <img src="./abbreve-logo.png" alt="Abbreve Logo" className="w-40 pt-10" />
       </div>
